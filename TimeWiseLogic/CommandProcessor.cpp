d4e36dc78cd20b8c4668d312fb1b5c3d0ea25ddf
Tran Hue Mai <tranhuemai@gmail.com>
2014-02-27 02:09:38 +0800

    CommandProcessor for ADD function (float tasks only)

--- TimeWiseLogic/CommandProcessor.cpp
+++ TimeWiseLogic/CommandProcessor.cpp
@@ -1,6 +1,5 @@
 #include "CommandProcessor.h"
-
-
+#include <string>
 CommandProcessor::CommandProcessor(void)
 {
 }
@@ -9,3 +8,18 @@ CommandProcessor::CommandProcessor(void)
 CommandProcessor::~CommandProcessor(void)
 {
 }
+
+void CommandProcessor::executeCommand(Command cmd) {
+	Task newTask = formTask(cmd);
+	_list.addTask(newTask);
+}
+
+Task CommandProcessor::formTask(Command cmd) {
+	Task temp;
+	std::string taskDescription = cmd.getDescription();
+	temp.addDescription(taskDescription);
+	return temp;
+}
+
+
+
--- TimeWiseLogic/CommandProcessor.h
+++ TimeWiseLogic/CommandProcessor.h
@@ -1,12 +1,21 @@
 #pragma once
-//To be done by Mai
+
+#ifndef _CommandProcessor_H
+#define _CommandProcessor_H
+
+#include "Command.h"
+#include "TaskList.h"
 class CommandProcessor
 {
 public:
 	CommandProcessor(void);
 	~CommandProcessor(void);
-
+	void executeCommand(Command);
+	Task formTask(Command);
+private:
+	TaskList _list;
 };
+#endif
 
 //Example
 /*

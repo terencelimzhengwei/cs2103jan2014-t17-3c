a79ee710f26a1b9254d8474b6613555a7161d4cf
JX <JX@localhost>
2014-04-02 22:42:42 +0800

    Set up feedback pop-up box to display feedback to user.

---
+++ TimeWiseGUI/timewisefeedback.h
@@ -0,0 +1,22 @@
+#ifndef TIMEWISEFEEDBACK_H
+#define TIMEWISEFEEDBACK_H
+
+#include <QWidget>
+#include <QPainter>
+#include "ui_timewisefeedback.h"
+
+class TimeWiseFeedback : public QWidget
+{
+	Q_OBJECT
+
+public:
+	TimeWiseFeedback(QWidget *parent = 0);
+	~TimeWiseFeedback();
+	void setData(const QString &labelText);
+	void paintEvent(QPaintEvent *pe);
+
+private:
+	Ui::TimeWiseFeedback ui;
+
+};
+#endif // TIMEWISEFEEDBACK_H

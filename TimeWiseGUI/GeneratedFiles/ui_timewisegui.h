0217688646aa2db08ff9cc4862adf58c80500392
JX <JX@localhost>
2014-04-06 17:40:18 +0800

    [V0.4] Overdue tasks turn red almost instantaneously (instead of every sec), Removed block throughout GUI.

--- TimeWiseGUI/GeneratedFiles/ui_timewisegui.h
+++ TimeWiseGUI/GeneratedFiles/ui_timewisegui.h
@@ -119,12 +119,12 @@ public:
 "color: rgb(0, 85, 255);"));
         label_help = new QLabel(centralWidget);
         label_help->setObjectName(QStringLiteral("label_help"));
-        label_help->setGeometry(QRect(61, 541, 489, 16));
-        label_help->setStyleSheet(QStringLiteral("font: 75 10pt \"New Cicle\";"));
+        label_help->setGeometry(QRect(50, 541, 491, 20));
+        label_help->setStyleSheet(QStringLiteral("font: 75 10.5pt \"New Cicle\";"));
         label_help->setWordWrap(true);
         userInput = new QLineEdit(centralWidget);
         userInput->setObjectName(QStringLiteral("userInput"));
-        userInput->setGeometry(QRect(60, 560, 471, 20));
+        userInput->setGeometry(QRect(50, 560, 491, 20));
         userInput->setStyleSheet(QStringLiteral("border-radius: 9px"));
         label_mlog = new QLabel(centralWidget);
         label_mlog->setObjectName(QStringLiteral("label_mlog"));
@@ -158,7 +158,7 @@ public:
         pushButton_minimize->setText(QString());
         pushButton_close->setText(QString());
         label_title->setText(QApplication::translate("TimeWiseGUIClass", "Your Tasks", 0));
-        label_help->setText(QApplication::translate("TimeWiseGUIClass", "You may: Add, Block, Clear, Confirm, Delete, Display, Done, Edit, Filter, Search, Undo, Redo", 0));
+        label_help->setText(QApplication::translate("TimeWiseGUIClass", "You may: Add, Clear, Confirm, Delete, Display, Done, Edit, Filter, Search, Undo, Redo", 0));
         userInput->setPlaceholderText(QApplication::translate("TimeWiseGUIClass", "+Add Task", 0));
         label_mlog->setText(QApplication::translate("TimeWiseGUIClass", "                      WELCOME TO...", 0));
     } // retranslateUi
